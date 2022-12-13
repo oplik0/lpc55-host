0cd2fcd8f615d8f24e4ec3003be9d7e31657c6de
oplik0 <opliko.reg@protonmail.com>
2022-12-13 15:44:54 +0100

    feat: callback struct

--- src/util.rs
+++ src/util.rs
@@ -1,4 +1,5 @@
 use core::convert::TryInto;
+use std::{sync::Arc, fmt::Debug};
 
 pub(crate) fn is_default<T: Default + PartialEq>(t: &T) -> bool {
     *t == Default::default()
@@ -98,4 +99,61 @@ pub fn word_padded(data: &[u8]) -> Vec<u8> {
     let mut data = Vec::from(data);
     word_pad(&mut data);
     data
+}
+
+#[derive(Clone)]
+pub struct CallbackFn<Args> {
+    pub cb: Arc<dyn Fn(Args)>
+}
+impl<Args> std::hash::Hash for CallbackFn<Args> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        state.write_usize(self.cb.as_ref() as *const _ as *const () as usize);
+    }
+}
+impl<Args>  PartialEq for CallbackFn<Args>  {
+    fn eq(&self, other: &Self) -> bool {
+        self.cb.as_ref() as *const _ as *const () == other.cb.as_ref() as *const _ as *const ()
+    }
+}
+impl<Args> Eq for CallbackFn<Args> {}
+
+impl<Args> PartialOrd for CallbackFn<Args> {
+    fn partial_cmp(&self, _other: &Self) -> Option<std::cmp::Ordering> {
+        None
+    }
+}
+impl<Args> Ord for CallbackFn<Args> {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        (self.cb.as_ref() as *const _ as *const () as usize).cmp(&(other.cb.as_ref() as *const _ as *const () as usize))
+    }
+
+    fn max(self, other: Self) -> Self
+    where
+        Self: Sized,
+    {
+        std::cmp::max_by(self, other, Ord::cmp)
+    }
+
+    fn min(self, other: Self) -> Self
+    where
+        Self: Sized,
+    {
+        std::cmp::min_by(self, other, Ord::cmp)
+    }
+
+}
+
+impl<Args> Debug for CallbackFn<Args> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("CallbackFn")
+            .field("cb", &(self.cb.as_ref() as *const _ as *const () as usize))
+            .finish()
+    }
+}
+impl<Args> Default for CallbackFn<Args> {
+    fn default() -> Self {
+        Self {
+            cb: Arc::new(|_: Args| {})
+        }
+    }
 }
